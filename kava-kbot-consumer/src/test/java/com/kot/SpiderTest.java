67ffe171387b3bd374bed54776123c1d632967ec
kris <kriscen@163.com>
2022-02-28 14:27:46 +0800

    去除jsonp相关依赖

--- kava-kbot-consumer/src/test/java/com/kot/SpiderTest.java
+++ kava-kbot-consumer/src/test/java/com/kot/SpiderTest.java
@@ -1,25 +1,20 @@
 package com.kot;
 
 import com.kbot.KbotApplication;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KbotApplication.class)
 public class SpiderTest {
 
-    @Test
-    public void weiboTest() throws IOException {
-        String url = "https://weibo.com/p/1002062286908003/home?from=page_100206&mod=TAB&is_all=1";
-        Document doc = Jsoup.connect(url).get();
-        System.out.println(doc.getElementsByAttribute("username"));
-        System.out.println(".......+");
-    }
+//    @Test
+//    public void weiboTest() throws IOException {
+//        String url = "https://weibo.com/p/1002062286908003/home?from=page_100206&mod=TAB&is_all=1";
+//        Document doc = Jsoup.connect(url).get();
+//        System.out.println(doc.getElementsByAttribute("username"));
+//        System.out.println(".......+");
+//    }
 
 }
